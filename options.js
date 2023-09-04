04b97a6ba1360157737f58a91f1c2453d0269c7d
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-09-04 22:44:22 +0100

    Update options.js

--- options.js
+++ options.js
@@ -1,1 +1,22 @@
+var userinput = document.getElementById('userinput');
 
+// User presses enter
+window.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        policyUpdate();
+    }
+});
+
+async function policyUpdate() {
+  const ruleIDs = await chrome.declarativeNetRequest.getEnabledRulesets();
+  await chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: ruleIDs});
+  userinput.value.
+  const domains = userinput.value.split(" ");
+  let id = 1;
+  for (let domain of domains) {
+    if (domain.length < 1) continue
+    chrome.declarativeNetRequest.updateDynamicRules({id: id, action: {type: 'block'}, condition: {requestDomains: [domain], excludedRequestDomains: [domain]}});
+    id += 1;
+  }
+}
