3aa43f86928129c7767ce50886df126b0a7ec0e1
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-09-08 00:52:48 +0100

    Update options.js

--- options.js
+++ options.js
@@ -1,18 +1,20 @@
+"use strict";
+
 const params = new URL(location.href).searchParams;
 const userinput = document.getElementById('userinput');
 const links = document.getElementById('links');
 const shortcutButton = document.getElementById('shortcutButton');
 const revokeButton = document.getElementById('revokeButton');
 const blockTypes = ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "webtransport", "webbundle", "other"];
+
 let ruleIDs = [];
-let ruleDomains = [];
 
 async function updateRules() {
     const rules = await chrome.declarativeNetRequest.getDynamicRules();
 
     links.innerHTML = '';
     ruleIDs = [];
-    ruleDomains = [];
+    let ruleDomains = [];
 
     for (let rule of rules) {
         ruleIDs.push(rule.id);
@@ -79,21 +81,7 @@ function createShortcut() {
         localStorage.setItem('secret', secret);
     }
     
-    prompt('Please use this URL', location.href + '?secret=' + localStorage.getItem('secret') + '&url=' + encodeURIComponent(url));
-}
-
-// Check if current URL is a redirect request.
-if (params.has('secret') && params.has('url')) {
-    redirecter(params.get('url'), params.get('secret'));
-}
-
-function redirecter(url, maybe_secret) {
-    const secret = localStorage.getItem('secret');
-    
-    // This page is not in WAR however its better to not increase the attack surface.
-    if (!secret || maybe_secret !== secret || invalidURL(url)) return
-
-    location.href = url;
+    prompt('Please use this URL', chrome.runtime.getURL('redirect.html') + '?secret=' + localStorage.getItem('secret') + '&url=' + encodeURIComponent(url));
 }
 
 function revokeShortcuts() {
