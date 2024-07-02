97b48cedd03c04322f13166e0df5c533f1c01961
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-07-02 16:25:14 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,18 +1,11 @@
 "use strict";
 
 const params = new URL(location.href).searchParams;
+redirecter(params.get('id'));
 
-// Check if current URL is a redirect request.
-if (params.has('secret') && params.has('url')) {
-    redirecter(params.get('url'), params.get('secret'));
-}
-
-function redirecter(url, maybe_secret) {
-    const secret = localStorage.getItem('secret');
-    
-    // This page is not in WAR however its better to not increase the attack surface.
-    if (!secret || maybe_secret !== secret || invalidURL(url)) return
-
+function redirecter(id) {
+    const url = localStorage.getItem('url_' + id);  
+    if (invalidURL(url)) return
     location.href = url;
 }
 
